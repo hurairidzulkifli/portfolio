8b25b1c013f06f3b79c391e493a28ae7f33ee86c
hurairidzulkifli <hurairi.dz@gmail.com>
2023-01-08 02:52:58 +0800

    initial commit

---
+++ pages/About.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container animate__animated animate__fadeIn">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="about">
+          <h1 class="line-after">About Me</h1>
+          <h4 class="mt-4">Hey 👋</h4>
+          <p>
+            My name is Hurairi Dzulkifli, I am a <b>backend developer </b> with <b>4</b> years of experience and
+            a part-time photographer wanna-be 🤣 .
+          </p>
+          <p>
+           Still hoping that one day i will create something with my skills and i'm 90% sure it will be <code>ecommerce</code> type of projects so fingers crossed !
+          </p>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <img
+          src="https://welcm.uk/storage/wink/images/BD9egpCzocsmrMW8qoxOdCX9r01474LxAY5Cm6eE.png"
+          alt="Itsuka Wallpaper"
+          class="abt-img"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      startYear: 2021,
+      currYear: new Date().getFullYear(),
+      stacks: [
+        {
+          language: "HTML",
+          experience: "3+ Years",
+          variant: "secondary",
+          breif: "lorem...",
+        },
+        {
+          language: "CSS",
+          experience: "3+ Years",
+          variant: "primary",
+          breif: "lorem...",
+        },
+        {
+          language: "Javascript",
+          experience: "2+ Years",
+          variant: "warning",
+          breif: "lorem...",
+        },
+        {
+          language: "VUE.JS",
+          experience: "2+ Years",
+          variant: "success",
+          breif: "lorem...",
+        },
+        {
+          language: "Node.js",
+          experience: "2+ Years",
+          variant: "dark",
+          breif: "lorem...",
+        },
+      ],
+    };
+  },
+  head: {
+    title: "About - HD",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        // currYear - startYear
+        content: `A Lightweight and Stable Discord Music Bot For Your Daily Production.`,
+      },
+    ],
+  },
+};
+</script>
+<style scoped>
+.container {
+  margin-top: 140px;
+  text-align: left;
+}
+.line-after {
+  overflow: hidden;
+}
+.line-after::after {
+  content: "";
+  display: inline-block;
+  height: 0.5em;
+  vertical-align: bottom;
+  width: 100%;
+  margin-right: -100%;
+  margin-left: 10px;
+  border-top: 2px solid rgb(211, 46, 46);
+}
+.abt-img {
+  width: inherit;
+  height: inherit;
+  border-radius: 5px;
+}
+.abt-img:hover {
+  animation: swing;
+  animation-duration: 2s;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,118 @@
+<template>
+  <b-row style="margin-left: 0; margin-right: 0">
+    <b-col md="4">
+      <b-img
+        src="https://avatars.githubusercontent.com/u/38495309?s=96&v=4"
+        id="avatar-img"
+        alt=""
+      />
+    </b-col>
+
+    <b-col md="8">
+      <h2 class="intro animate__animated animate__fadeInUp animate__fast">
+        Hi!,
+        <br />I<span>'m Hurairi Dzulkifli</span>.
+      </h2>
+
+      <div class="col-md-10 info">
+        A <b> Backend Developer </b> who specializes in the development of server-side web applications using the <b> Laravel </b> web application framework and a bit of <b>Vue</b> .
+        <br />
+        I solemnly swear i am fun to work with 😁.
+        <br />
+        <b-button
+          class="action-btn animate__animated animate__wobble animate__delay-4s animate__fast"
+          to="/about"
+        >
+          Explore >
+        </b-button>
+
+        <b-button
+          class="action-btn"
+          href="https://www.linkedin.com/in/hurairi-dzulkifli/"
+          variant="primary"
+          target="_blank"
+        >
+          Get In Touch
+        </b-button>
+
+        <!-- .SOCIAL LINKS SECTION -->
+        <div
+          class="social-icons animate__animated animate__fadeInUp animate__delay-1s"
+        >
+          <social-link :to="socialLinks.github">
+            <GithubIcon />
+          </social-link>
+
+          <social-link :to="socialLinks.linkedin">
+            <LinkedinIcon />
+          </social-link>
+        </div>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import GithubIcon from "vue-ionicons/dist/logo-github.vue";
+import LinkedinIcon from "vue-ionicons/dist/logo-linkedin.vue";
+import TwitterIcon from "vue-ionicons/dist/logo-twitter.vue";
+import FacebookIcon from "vue-ionicons/dist/logo-facebook.vue";
+import YoutubeIcon from "vue-ionicons/dist/logo-youtube.vue";
+import MailIcon from "vue-ionicons/dist/md-mail.vue";
+
+export default {
+  components: {
+    GithubIcon,
+    LinkedinIcon,
+    TwitterIcon,
+    FacebookIcon,
+    YoutubeIcon,
+    MailIcon,
+  },
+
+  data() {
+    return {
+      avatar: require("@/assets/avatar-1.webp"),
+      // ADD YOUR SOCIAL LINKS HERE 👇
+      socialLinks: {
+        github: "https://github.com/hurairidzulkifli",
+        linkedin: "https://www.linkedin.com/in/hurairi-dzulkifli/",
+      },
+    };
+  },
+
+  head: {
+    title: "HD Dashbord",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "A Lightweight and Stable Discord Music Bot For Your Daily Production.",
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        content: "HD Dashbord",
+      },
+      {
+        property: "og:description",
+        content:
+          "A Lightweight and Stable Discord Music Bot For Your Daily Production.",
+      },
+      {
+        hid: "og:image",
+        name: "og:image",
+        content: "https://i.imgur.com/UmWcMjO.png",
+      },
+    ],
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+
+      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+    });
+  },
+};
+</script>
